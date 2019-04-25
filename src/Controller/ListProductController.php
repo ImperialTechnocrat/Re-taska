bef361637f34476645a6722454252eca6f10375f
ImperialTechnocrat <juan.denagykallo@gmail.com>
2019-04-25 20:31:30 +0200

    adminer

---
+++ src/Controller/ListProductController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ListProductController extends AbstractController
+{
+    /**
+     * @Route("/list/product", name="list_product")
+     */
+    public function index()
+    {
+        return $this->render('list_product/index.html.twig', [
+            'controller_name' => 'ListProductController',
+        ]);
+    }
+}
---
+++ src/Entity/Product.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ProductRepository")
+ */
+class Product
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Detail;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->Name;
+    }
+
+    public function setName(string $Name): self
+    {
+        $this->Name = $Name;
+
+        return $this;
+    }
+
+    public function getDetail(): ?string
+    {
+        return $this->Detail;
+    }
+
+    public function setDetail(string $Detail): self
+    {
+        $this->Detail = $Detail;
+
+        return $this;
+    }
+}
